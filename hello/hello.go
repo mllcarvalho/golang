97512487d9b06535241dadd16da3d5325d588304
Matheus Llobregat Carvalho <mllcarvalho.it@gmail.com>
2024-05-07 16:33:58 -0300

    feat: start go

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"hello/mypackage"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	mypackage.PrintHello()
+	fmt.Println("😍🍨")
+	fmt.Println(`
+	A Necessary Autumn Inside Each
+	~Rumi~
+
+	You and I have spoken all these words,
+	but as for the way we have to go,
+	words are no preparation.
+	There is no getting ready, other than grace....
+	Inside each of us, there’s continual autumn.
+	Our leaves fall and are blown out over the water.
+	A crow sits in the blackened limbs
+	and talks about what’s gone....
+	There’s a necessary dying,
+	and then Jesus is breathing again.
+	Very little grows on jagged rock.
+	Be ground. Be crumbled, so wildflowers
+	will come up where you are.
+	You’ve been stony for too many years.
+	Try something different.
+	Surrender.
+	`)
 }
